56d5519ca9306b3b2e762b78b5e9f129137d4cd8
Iulia Stremciuc <iuliastremciuc@gmail.com>
2020-03-17 23:02:19 -0500

    titanic data'' errors

---
+++ module2-sql-for-analysis/titanic.py
@@ -0,0 +1,66 @@
+import pandas 
+import psycopg2
+import os
+from dotenv import load_dotenv
+import json
+from psycopg2.extras import execute_values
+import sqlite3
+load_dotenv() 
+df = pandas.read_csv('titanic.csv')
+DB_NAME = os.getenv("DB_NAME", default="OOPS")
+DB_USER = os.getenv("DB_USER", default="OOPS")
+DB_PASSWORD = os.getenv("DB_PASSWORD", default="OOPS")
+DB_HOST = os.getenv("DB_HOST", default="OOPS")
+
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER,
+                        password=DB_PASSWORD, host=DB_HOST)
+cur = conn.cursor()                        
+print("Connection:", conn)
+print("Cursor: ", cur)
+print(df.head(5))
+print(df.columns)
+
+print(df.loc[[0]])
+#iterrows()
+#data = [row for row in df.iterrows()]
+## for row in data: 
+## curs.execute(my_query, row)
+
+query = """
+CREATE TABLE IF NOT EXISTS titanic(
+    id serial primary key,
+    Survived int not null,
+    Pclass int not null,
+    Name text not null,
+    Sex text not null,
+    Age float not null,
+    Siblings_Spouses_Aboard int not null,
+    Parents_Children_Aboard int not null,
+    Fare real not null
+);
+"""
+cur.execute(query)
+cur.execute("SELECT * FROM titanic;")
+result = cur.fetchall()
+print("Result: ", len(result))
+
+data = list(df.iterrows())
+
+insert_query = """INSERT INTO titanic(Survived, Pclass, Name, Sex, Age, Siblings_Spouses_Aboard, Parents_Children_Aboard, Fare) 
+    VALUES(%s, %s, %s, %s, %s, %s, %s, %s)
+    
+    """
+for row in data:
+    cur.execute(insert_query, row)
+
+#titanic_list = list(df.iterrows())
+#execute_values(cur, insert_query, titanic_list)
+
+
+cur.execute("SELECT * FROM titanic;")
+result = cur.fetchall()
+
+print("Result: ", len(result))
+
+
+
